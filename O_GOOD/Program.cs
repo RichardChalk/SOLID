207685f3c08f9c775087db494154acdb2df6ab32
Richard Chalk <barmybrit@gmail.com>
2023-10-19 06:54:27 +0200

    Added triangle to O

--- O_GOOD/Program.cs
+++ O_GOOD/Program.cs
@@ -6,11 +6,13 @@
         {
             IShape rectangle = new Rectangle { Width = 5, Height = 10 };
             IShape circle = new Circle { Radius = 5 };
+            IShape triangle = new Triangle { Base = 5, Height = 10 };
 
             AreaCalculator calculator = new AreaCalculator();
 
             Console.WriteLine($"Area of Rectangle: {calculator.CalculateArea(rectangle)}");
             Console.WriteLine($"Area of Circle: {calculator.CalculateArea(circle)}");
+            Console.WriteLine($"Area of Triangle: {calculator.CalculateArea(triangle)}");
 
             Console.ReadLine();
         }
